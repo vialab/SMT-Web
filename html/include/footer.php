fd24c539d139e26da0215e726f85a9a73b77043e
KiwiStrongis <kiwistrongis@gmail.com>
2014-02-21 17:11:08 -0500

    alphabetical orderized

--- html/include/footer.php
+++ html/include/footer.php
@@ -6,8 +6,8 @@
 				<a href="http://www.erikpaluka.com">Erik Paluka</a>, 
 				<a href="http://vialab.science.uoit.ca/portfolio/kalev-kalda-sikes">Kalev Kalda Sikes</a>, 
 				<a href="http://vialab.science.uoit.ca/portfolio/zachary-cook">Zachary Cook</a>, 
-				<a href="http://touchlab.uwaterloo.ca/">Dr. Mark Hancock</a>, 
-				<a href="http://vialab.science.uoit.ca">Dr. Christopher Collins</a>
+				<a href="http://vialab.science.uoit.ca">Dr. Christopher Collins</a>,
+				<a href="http://touchlab.uwaterloo.ca/">Dr. Mark Hancock</a>,
 			</p>
 		</footer>
 	</div>
