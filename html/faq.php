dd8351ad206000ab013ad73e34faa1f9011c378a
paluka <erik.paluka@gmail.com>
2014-02-27 15:01:19 -0500

    Working on adding faq and showcase pages

---
+++ html/faq.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		$title = "[FAQ]";
+		$containsCode = true;
+		include 'include/head.php';
+	?>
+</head>
+<body>
+	<?php
+		$thisPage = "FAQ";
+		include 'include/header.php';
+	?>
+	<!--Start of this page's content-->
+		<h2>FAQ</h2>
+		<p>Here are some of the most frequently asked questions concerning SMT.
+		</p>
+    
+	<!--End of this page's content-->
+	<?php
+		include 'include/footer.php';
+	?>
+</body>
+</html>
---
+++ html/showcase.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		$title = "[Showcase]";
+		$containsCode = true;
+		include 'include/head.php';
+	?>
+</head>
+<body>
+	<?php
+		$thisPage = "Showcase";
+		include 'include/header.php';
+	?>
+	<!--Start of this page's content-->
+		<h2>Showcase</h2>
+		<p>Here are some projects that have been built using the Simple Multi-Touch Toolkit.
+		</p>
+	<!--End of this page's content-->
+	<?php
+		include 'include/footer.php';
+	?>
+</body>
+</html>
