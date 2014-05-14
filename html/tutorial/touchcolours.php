3765e4f43d21e53089dd4dab7ed5cf2bb328b7ff
kiwistrongis <kiwistrongis@gmail.com>
2014-05-14 17:24:54 -0400

    worked on the touch colours tutorial

--- html/tutorial/touchcolours.php
+++ html/tutorial/touchcolours.php
@@ -21,6 +21,12 @@
 			This is a more advanced tutorial. It assumes that the reader knows basic processing and has read the introductory tutorials.
 		</p>
 		<p>
+			The default touch drawer, as of SMT 4.0, is the textured touch drawer. This tutorial is only about the textured touch drawer. If you're encountering performance issues, there are a number of things that can be done to help that will be discussed later. However it should also be known that there are other options for touch drawing, namely the smooth and debug methods, as well as disabled touch drawing and using a custom touch drawer class. These options can be enabled by calling <code>SMT.setTouchDraw( TouchDraw.SMOOTH);</code>, <code>SMT.setTouchDraw( TouchDraw.DEBUG);</code>, <code>SMT.setTouchDraw( TouchDraw.NONE);</code>, and <code>SMT.setTouchDraw( TouchDraw.CUSTOM, TouchDrawer drawer);</code>, respectively. None of the touch drawing customization functions discussed in the rest of this tutorial apply to any of those touch drawing methods. These functions only work when using the textured touch drawer. If you change the touch drawing method, and for some reason want to change back to the textured touch drawer, call <code>SMT.setTouchDraw( TouchDraw.TEXTURED);</code>.
+		</p>
+		<p>
+			With all that said, the textured touch drawer will most likely suit your needs. You can customize touch colours, radius, death animation duration, trail colours, trail width, trail length, trail tightness, and trail smoothness. You can also set the number of triangles used to draw touches, the number of rectangles used to draw the trail, the maximum number of sample points used to interpolate the trail, and the length in time of the trail. Additionally, you can set individual touch's colours and trail colours, which will override the globally set colours. Finally, trails can be disabled.
+		</p>
+		<p>
 			asdf
 		</p>
 
